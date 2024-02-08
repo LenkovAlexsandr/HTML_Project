555c924568ba39408882b744e0078d625f635609
mapProject <lenkovsaschka@gmail.com>
2024-02-08 17:14:55 +0300

    Тренировки в полёте

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def training(prof):
     return render_template('training.html', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    if list not in ['ol', 'ul']:
+        return 'Error'
+    list_pr = ['a', 'b', 'c', 'd']
+    return render_template('list_prof.html', list=list, list_pr=list_pr)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
 
