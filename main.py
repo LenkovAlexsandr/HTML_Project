77a6ced46b61613021d9969cb10280cb73c2503f
mapProject <lenkovsaschka@gmail.com>
2024-02-08 16:58:03 +0300

    Тренировки в полёте

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
 
